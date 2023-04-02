6657f302ae838a3d7c89a2550d133940f01a0f80
Sebastian Wehner <Basti.wehner@googlemail.com>
2023-04-02 17:22:15 +0200

    initial commit

---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import "@picocss/pico/css/pico.min.css"
+</script>
+
+<style>
+    nav { border-bottom: thin solid turquoise; }
+</style>
+
+<nav class="container-fluid">
+    <ul>
+        <li>Agile Analytix</li>
+    </ul>
+    <ul>
+        <li><a href="/app">Home</a></li>
+        <li><a href="/app/profile">Profile</a></li>
+    </ul>
+</nav>
+<slot />
---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,3 @@
+<div class="container-fluid">
+    <h1>Overview</h1>
+</div>
