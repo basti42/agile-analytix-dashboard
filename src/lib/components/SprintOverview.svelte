af190048ce2acceef18d82be8482bac7fc9df861
basti <basti.wehner@googlemail.com>
2023-05-07 11:37:29 +0200

    feat: added bug_count column to sprint overview

--- src/lib/components/SprintOverview.svelte
+++ src/lib/components/SprintOverview.svelte
@@ -12,21 +12,26 @@
             <tr>
                 <th scope="col">#</th>
                 <th scope="col">Name</th>
-                <th scope="col">Velocity</th>
+                <th scope="col">Velocity/Forecast</th>
                 <th scope="col">#Developers</th>
+                <th scope="col">#Bugs</th>
             </tr>
         </thead>
         <tbody>
-            { #each sprintResponse.elements as {name, velocity, developers}, i }
+            { #each sprintResponse.elements as {name, velocity, forecast, developers, bug_count}, i }
             <tr>
                 <th scope="row">{sprintResponse.number_of_elements - i}</th>
                 <td>{name}</td>
-                <td>{velocity}</td>
+                <td>{velocity} / <small>{forecast}</small></td>
                 <td>{developers}</td>
+                <td>{bug_count}</td>
             </tr>    
         { /each }
         </tbody>
     </table>
 
-
 </div>
+
+<style>
+    small { filter: brightness(70%); }
+</style>
