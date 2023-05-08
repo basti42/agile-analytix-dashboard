2e8dc270771332d10e99904311de1526db71ae0c
basti <basti.wehner@googlemail.com>
2023-05-08 19:09:59 +0200

    feat: small changes to plotting

--- src/lib/components/VelocityForecastChart.svelte
+++ src/lib/components/VelocityForecastChart.svelte
@@ -11,18 +11,36 @@
         return Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
     }
 
+    function sma(arr: Array<number>, window_size: number) : Array<number> {
+        let res: Array<number> = [];
+        for (let i = window_size; i < arr.length; i ++){
+            res.push(arr.slice(i-window_size, i).reduce((acc, element) => acc + element, 0.0) / window_size);
+        }
+        // for (let j=0; j<window_size; j++) { res.unshift(null); }
+        return res;
+    }
+
+    function calc_sma(vel: Array<number>, fore: Array<number>, names: Array<string>) {
+        let sma_velocity = sma(vel, 3);
+        let sma_forecast = sma(fore, 3);
+        let sma_labels = names.slice(3, names.length);
+        return {sma_velocity, sma_forecast, sma_labels};
+    }
+
     onMount(() => {
         const node = document.getElementById("velocity_forecast_chart");
         const names = sprints.map((a) => a.name).reverse();
         const velocity_values = sprints.map((a) => a.velocity).reverse();
-        const velocity_total = sprints.reduce((acc, s) => acc + s.velocity, 0.0);
-        const velocity_mean = velocity_total / velocity_values.length;
         const forecast_values = sprints.map((a) => a.forecast).reverse();
-        const forecast_total = sprints.reduce((acc, s) => acc + s.forecast, 0.0);
-        const forecast_mean = forecast_total / forecast_values.length;
+
+        const sma_result = calc_sma(velocity_values, forecast_values, names);
+
         if (node != null){
             echarts.init(node).setOption(
             {
+                grid: {
+                    containLabel: true,
+                },
                 legend: {
                     orient: 'horizontal',
                     left: 'center',
@@ -31,7 +49,12 @@
                 },
                 xAxis: {
                     type: 'category',
-                    data: names
+                    data: names,
+                    axisLabel: {
+                        show: true,
+                        interval: 0,
+                        rotate: 45
+                    }
                 },
                 yAxis: {
                     type: 'value'
@@ -42,11 +65,24 @@
                         data: velocity_values,
                         type: 'line'
                     },
+                    // {
+                    //     name: 'Velocity SMA',
+                    //     data: sma_result.sma_velocity,
+                    //     type: 'line',
+                    //     linestyle: {color: '#515151'}
+
+                    // },
                     {
                         name: "Forecast",
                         data: forecast_values,
                         type: 'line'
-                    }
+                    },
+                    // {
+                    //     name: "Forecast SMA",
+                    //     data: sma_result.sma_forecast,
+                    //     type: 'line',
+                    //     linestyle: {color: '#515151'}
+                    // }
                 ]
             });
         } else {
@@ -60,7 +96,7 @@
 
 <style>
     #velocity_forecast_chart {
-        width: 90%;
-        height: 40vh;
+        width: 100%;
+        height: 50vh;
     }
 </style>
